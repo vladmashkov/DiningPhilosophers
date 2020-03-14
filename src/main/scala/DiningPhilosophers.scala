1fd744f49aac41c5db743f3217526a5d076623d3
vlad_mashkokv <vladmashkov99@list.ru>
2020-03-14 20:17:33 +0300

    Commit

--- src/main/scala/DiningPhilosophers.scala
+++ src/main/scala/DiningPhilosophers.scala
@@ -1,86 +1,85 @@
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Future}
+import scala.concurrent.{Await, Future}
+
 trait Philosopher {
   val name: String
 
-  final def takeLeft: Unit = println(s"$name is take left fork!")
-
-  final def takeRight: Unit = println(s"$name is take Right fork!")
-
-  final def live: Unit = {
-    takeLeft
-
-    {
-      takeRight
-
-      {
-        eat
-      }
-    }
-  }
-
-  final def eat: Unit = println(s"$name is eating...")
-}
-
-class Aristotle extends Philosopher {
-  override val name: String = "Aristotle"
-}
-
-class Plato extends Philosopher {
-  override val name: String = "Plato"
-}
-
-class Confucius extends Philosopher {
-  override val name: String = "Confucius"
-}
-
-class Socrates extends Philosopher {
-  override val name: String = "Socrates"
-}
-
-class Machiavelli extends Philosopher {
-  override val name: String = "Machiavelli"
-}
-
-object Main extends App {
   final val fork_1 = 1
   final val fork_2 = 2
   final val fork_3 = 3
   final val fork_4 = 4
   final val fork_5 = 5
+
+  val withLeft: Int
+  val withRight: Int
+
+  final def takeLeft(body: => Unit): Unit = {
+    withLeft.synchronized {
+      println(s"$name is take left fork $withLeft!")
+      body
+    }
+  }
+
+  final def takeRight(body: => Unit): Unit = {
+    withRight.synchronized {
+      println(s"$name is take Right fork $withRight!")
+      body
+    }
+  }
+
+  final def live: Unit = {
+    takeLeft {
+      takeRight {
+        eat
+      }
+    }
+  }
+  final def eat: Unit = println(s"$name is eating...")
+}
+class Aristotle extends Philosopher {
+  override val name: String = "Aristotle"
+  override val withLeft: Int = fork_1
+  override val withRight: Int = fork_2
+}
+class Plato extends Philosopher {
+  override val name: String = "Plato"
+  override val withLeft: Int = fork_2
+  override val withRight: Int = fork_3
+}
+class Confucius extends Philosopher {
+  override val name: String = "Confucius"
+  override val withLeft: Int = fork_3
+  override val withRight: Int = fork_4
+}
+class Socrates extends Philosopher {
+  override val name: String = "Socrates"
+  override val withLeft: Int = fork_4
+  override val withRight: Int = fork_5
+}
+class Machiavelli extends Philosopher {
+  override val name: String = "Machiavelli"
+  override val withLeft: Int = fork_1
+  override val withRight: Int = fork_5
+}
+object Main extends App {
   val aristotle = new Aristotle
   val plato = new Plato
   val confucius = new Confucius
   val socrates = new Socrates
   val machiavelli = new Machiavelli
-
   val aristotleLive = Future {
-    fork_1.synchronized{
-      fork_2.synchronized(aristotle.live)
-    }
+    aristotle.live
   }
-
   val platoLive = Future {
-    fork_2.synchronized {
-      fork_3.synchronized(plato.live)
-    }
+    plato.live
   }
-
   val confuciusLive = Future {
-    fork_3.synchronized {
-      fork_4.synchronized(confucius.live)
-    }
+    confucius.live
   }
-
   val socratesLive = Future {
-    fork_4.synchronized {
-      fork_5.synchronized(socrates.live)
-    }
+    socrates.live
   }
-
   val machiavelliLive = Future {
-    fork_5.synchronized {
-      fork_1.synchronized(machiavelli.live)
-    }
+    machiavelli.live
   }
 }
